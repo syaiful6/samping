ab49cd8010a57b45ec44229eefe0e89054b2198b
Syaiful Bahri <syai.bahri6@gmail.com>
2025-01-26 20:32:52 +0700

    fix task timeout

--- example/app.py
+++ example/app.py
@@ -21,6 +21,7 @@ app = App(
     default_queue="samping",
     disable_cron=True,
     queue_size=60,
+    task_timeout=30,
 )
 app.routes = [
     Rule("test_*", "samping"),
@@ -41,7 +42,15 @@ async def buggy_task(wait: int):
     raise RuntimeError("buggy")
 
 
+@app.task(name="long_running_tasks")
+async def long_running_tasks():
+    logger.info("executing long running tasks")
+    await asyncio.sleep(60)
+
+
 @app.tab(name="print_each_minute")
 async def print_each_minute():
     logger.info("crontab print each minute executed")
     await asyncio.sleep(1)
+
+    
